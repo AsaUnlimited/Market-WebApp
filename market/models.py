9790c6a31d18c12707367b1d400bbd9ef5e8f657
AsaUnlimited <ayinladesikiru@gmail.com>
2022-07-02 07:10:27 +0100

    secured logout implemented

--- market/models.py
+++ market/models.py
@@ -14,12 +14,13 @@ class User(db.Model, UserMixin):
     budget = db.Column(db.Integer(), nullable=False, default=100)
     items = db.relationship('Item', backref='owned_user', lazy=True)
 
-    # @property
-    # def prettier_budget(self):
-    #     if len(str(self.budget)) >= 4:
-    #
-    #     else:
-    #         return f"${self.budget}"
+    @property
+    def prettier_budget(self):
+        if len(str(self.budget)) >= 4:
+            return f'${str(self.budget)[:-3]},{str(self.budget)[-3]}'
+
+        else:
+            return f"${self.budget}"
 
     @property
     def password(self):
