d7ab108c2083ae14262f67211ea25f706ae8165b
AsaUnlimited <ayinladesikiru@gmail.com>
2022-07-03 06:48:58 +0100

    user can put purchased item back to implemented

--- market/forms.py
+++ market/forms.py
@@ -15,14 +15,20 @@ class RegisterForm(FlaskForm):
         if email_address:
             raise ValidationError('Email already exist!, try a different email. stupid!')
 
-    username = StringField(label='User Name:', validators=[Length(min=6, max= 25), DataRequired()])
+    username = StringField(label='User Name:', validators=[Length(min=3, max= 25), DataRequired()])
     email_address = StringField(label='Email Address:', validators=[Email(), DataRequired()])
-    password1 = PasswordField(label='Password', validators=[Length(min=8), DataRequired()])
+    password1 = PasswordField(label='Password', validators=[Length(min=6), DataRequired()])
     password2 = PasswordField(label='Confirm Password', validators=[EqualTo('password1'), DataRequired()])
     submit = SubmitField(label='Create Account')
 
 
 class LoginForm(FlaskForm):
     username = StringField(label='User Name:', validators=[DataRequired()])
-    password = StringField(label='Password:', validators=[DataRequired()])
+    password = PasswordField(label='Password:', validators=[DataRequired()])
     submit = SubmitField(label='Sign In')
+
+class PurchaseItemForm(FlaskForm):
+    submit = SubmitField(label='Purchase Item')
+
+class SellItemForm(FlaskForm):
+    submit = SubmitField(label='Sell Item')
